3e050db9abc6515c9c3d0690b91bbd2a1e57f020
FelipeAF2004 <felipe.afonso@tecniwerservice.com.br>
2026-05-08 08:25:00 -0300

    UNico comit

---
+++ script.js
@@ -0,0 +1,18 @@
+function mensagem() {
+   let pergunta1 = Number(prompt("Bem-vinda ao site do Felipe\nDigite 1 para continuar"))
+
+   while (pergunta1 != 1) {
+      alert("Refaça")
+      pergunta1 = Number(prompt("Digite 1 para continuar"))
+   }
+
+  let pergunta2 = prompt("Qual a data do nosso aniversário? EX: 3/5")
+  let pergunta3 = prompt("Qual a minha cor favorita? EX: PRETO. deve ser escrito tudo maisculo" )
+
+  if(pergunta2 == "2/8" && pergunta3 == "AZUL"){
+    document.getElementById("foto").src = "https://i.pinimg.com/236x/1e/16/67/1e1667028c4a1340a1f61d8f30dca039.jpg"
+  }
+
+}
+
+mensagem()
